5fac61e0733c58f2285c64f1980cf3d251da2d1d
Savkela <nikolaakv@gmail.com>
2022-11-22 18:02:25 +0100

    feat: delete expired events

--- social-network/src/main/java/com/levi9/socialnetwork/Service/impl/EventServiceImpl.java
+++ social-network/src/main/java/com/levi9/socialnetwork/Service/impl/EventServiceImpl.java
@@ -1,9 +1,11 @@
 package com.levi9.socialnetwork.Service.impl;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 import com.levi9.socialnetwork.Exception.ResourceExistsException;
 import com.levi9.socialnetwork.Exception.ResourceNotFoundException;
@@ -25,43 +27,6 @@ public class EventServiceImpl implements EventService {
 	@Autowired
 	private EventRepository eventRepository;
 
-	public List<Event> getAllEvents() {
-		return eventRepository.findAll();
-	}
-
-	public Event getEventById(Long eventId) throws ResourceNotFoundException {
-		return eventRepository.findById(eventId)
-				.orElseThrow(() -> new ResourceNotFoundException(NOT_FOUND_MESSAGE + eventId));
-	}
-
-	public Event createEvent(Event event) throws ResourceExistsException {
-		Long eventId = event.getId();
-		if (eventId != null && eventRepository.existsById(eventId)) {
-			throw new ResourceExistsException(ALREADY_EXISTS_MESSAGE + eventId);
-		}
-		return eventRepository.save(event);
-	}
-
-	public Event updateEvent(Long eventId, Event eventDetails) throws ResourceNotFoundException {
-		Event event = eventRepository.findById(eventId)
-				.orElseThrow(() -> new ResourceNotFoundException(NOT_FOUND_MESSAGE + eventId));
-
-		event.setStartDate(eventDetails.getStartDate());
-		event.setEndDate(eventDetails.getEndDate());
-
-		return eventRepository.save(event);
-	}
-
-	public void deleteEvent(Long eventId) throws ResourceNotFoundException {
-		Event event = eventRepository.findById(eventId)
-				.orElseThrow(() -> new ResourceNotFoundException(NOT_FOUND_MESSAGE + eventId));
-
-	
-    private static final String NOT_FOUND_MESSAGE = "Event not found for this id :: ";
-    private static final String ALREADY_EXISTS_MESSAGE = "Event already exists with this id :: ";
-
-    @Autowired
-    private EventRepository eventRepository;
 
     public List<Event> getAllEvents() {
         return eventRepository.findAll();
