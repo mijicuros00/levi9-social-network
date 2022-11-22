0e2a88aebb337b1e3cf3a67a87976717e5addba0
mijicuros00 <urosmijic00@gmail.com>
2022-11-22 14:23:59 +0100

    fix: Changed data in insert SQL file

--- social-network/src/main/resources/dblogs/changelog/changes/15-insert-user-table-changeset.sql
+++ social-network/src/main/resources/dblogs/changelog/changes/15-insert-user-table-changeset.sql
@@ -1,10 +1,11 @@
-insert into "user" (id, name, surname, email, password, status) values (default, 'Tadeo', 'Cawthery', 'tcawthery0@behance.net', '2O8q85MTqjtG', 'VERIFIED');
-insert into "user" (id, name, surname, email, password, status) values (default, 'Cara', 'Sabin', 'csabin1@amazon.de', 'svQUCJ4ZQFAq', 'VERIFIED');
-insert into "user" (id, name, surname, email, password, status) values (default, 'Maisie', 'Peepall', 'nikolaakv@gmail.com', 'LzfttHhhk9', 'VERIFIED');
-insert into "user" (id, name, surname, email, password, status) values (default, 'Ruggiero', 'Boij', 'rboij3@imgur.com', '1L9yqB', 'VERIFIED');
-insert into "user" (id, name, surname, email, password, status) values (default, 'Caz', 'Gavrieli', 'cgavrieli4@woothemes.com', 'Jix1T8R7AqXu', 'VERIFIED');
-insert into "user" (id, name, surname, email, password, status) values (default, 'Correy', 'Klimentyonok', 'cklimentyonok5@businessinsider.com', 'hkDVWwl', 'VERIFIED');
-insert into "user" (id, name, surname, email, password, status) values (default, 'Doloritas', 'Bonafacino', 'dbonafacino6@mapy.cz', 'tvF3HIVB6JtY', 'VERIFIED');
-insert into "user" (id, name, surname, email, password, status) values (default, 'Lu', 'Uren', 'luren7@nhs.uk', 'nBBV66Sl9u', 'VERIFIED');
-insert into "user" (id, name, surname, email, password, status) values (default, 'Talyah', 'Czaja', 'tczaja8@networksolutions.com', '9pDwZJmlP', 'VERIFIED');
-insert into "user" (id, name, surname, email, password, status) values (default, 'Dalston', 'MacCartan', 'dmaccartan9@so-net.ne.jp', 'YmwO27', 'VERIFIED');
+insert into "user" (id, name, surname, email, password, status) values (default, 'Tadeo', 'Cawthery', 'tcawthery0@behance.net', '$2a$10$.r4El/SxWktyti7tzqKnDuv.nAZGW2XK6mD5lZZYxMSQsdF.uMWRq', 'VERIFIED');
+insert into "user" (id, name, surname, email, password, status) values (default, 'Cara', 'Sabin', 'csabin1@amazon.de', '$2a$10$.r4El/SxWktyti7tzqKnDuv.nAZGW2XK6mD5lZZYxMSQsdF.uMWRq', 'VERIFIED');
+insert into "user" (id, name, surname, email, password, status) values (default, 'Maisie', 'Peepall', 'nikolaakv@gmail.com', '$2a$10$.r4El/SxWktyti7tzqKnDuv.nAZGW2XK6mD5lZZYxMSQsdF.uMWRq', 'VERIFIED');
+insert into "user" (id, name, surname, email, password, status) values (default, 'Ruggiero', 'Boij', 'rboij3@imgur.com', '$2a$10$.r4El/SxWktyti7tzqKnDuv.nAZGW2XK6mD5lZZYxMSQsdF.uMWRq', 'VERIFIED');
+insert into "user" (id, name, surname, email, password, status) values (default, 'Caz', 'Gavrieli', 'cgavrieli4@woothemes.com', '$2a$10$.r4El/SxWktyti7tzqKnDuv.nAZGW2XK6mD5lZZYxMSQsdF.uMWRq', 'VERIFIED');
+insert into "user" (id, name, surname, email, password, status) values (default, 'Correy', 'Klimentyonok', 'cklimentyonok5@businessinsider.com', '$2a$10$.r4El/SxWktyti7tzqKnDuv.nAZGW2XK6mD5lZZYxMSQsdF.uMWRq', 'VERIFIED');
+insert into "user" (id, name, surname, email, password, status) values (default, 'Doloritas', 'Bonafacino', 'dbonafacino6@mapy.cz', '$2a$10$.r4El/SxWktyti7tzqKnDuv.nAZGW2XK6mD5lZZYxMSQsdF.uMWRq', 'VERIFIED');
+insert into "user" (id, name, surname, email, password, status) values (default, 'Lu', 'Uren', 'luren7@nhs.uk', '$2a$10$.r4El/SxWktyti7tzqKnDuv.nAZGW2XK6mD5lZZYxMSQsdF.uMWRq', 'VERIFIED');
+insert into "user" (id, name, surname, email, password, status) values (default, 'Talyah', 'Czaja', 'tczaja8@networksolutions.com', '$2a$10$.r4El/SxWktyti7tzqKnDuv.nAZGW2XK6mD5lZZYxMSQsdF.uMWRq', 'VERIFIED');
+insert into "user" (id, name, surname, email, password, status) values (default, 'Dalston', 'MacCartan', 'dmaccartan9@so-net.ne.jp', '$2a$10$.r4El/SxWktyti7tzqKnDuv.nAZGW2XK6mD5lZZYxMSQsdF.uMWRq', 'VERIFIED');
+--All passwords are 123123
