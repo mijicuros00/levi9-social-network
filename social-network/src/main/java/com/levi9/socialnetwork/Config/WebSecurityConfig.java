db571f9ff956c2e15dc83c130cb7f84be9f0a980
AleksaPapovic <papovic.ra167.2018@uns.ac.rs>
2022-11-23 15:28:43 +0100

    feat: add swagger

--- social-network/src/main/java/com/levi9/socialnetwork/Config/WebSecurityConfig.java
+++ social-network/src/main/java/com/levi9/socialnetwork/Config/WebSecurityConfig.java
@@ -86,6 +86,13 @@ public class WebSecurityConfig {
 		
 		return http.build();
 	}
+	
+	private static final String[] AUTH_WHITELIST = {
+	        "/swagger-resources/**",
+	        "/swagger-ui.html",
+	        "/v2/api-docs",
+	        "/webjars/**"
+	};
 
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
@@ -93,6 +100,7 @@ public class WebSecurityConfig {
 			web.ignoring().antMatchers(HttpMethod.POST, "/auth/login");
 			web.ignoring().antMatchers(HttpMethod.GET, "/", "/webjars/**", "/*.html", "favicon.ico", "/**/*.html",
 					"/**/*.css", "/**/*.js");
+			 web.ignoring().antMatchers(AUTH_WHITELIST);
 		};
 	}
 
