03de4fc850d91f2802f4d829c9b1e5aaf30f1614
AleksaPapovic <papovic.ra167.2018@uns.ac.rs>
2022-11-30 14:48:26 +0100

    feat: add api key

--- social-network/src/main/resources/dblogs/changelog/changes/14-add-constraints-changeset.sql
+++ social-network/src/main/resources/dblogs/changelog/changes/14-add-constraints-changeset.sql
@@ -48,6 +48,8 @@ ALTER TABLE "post_item" ADD FOREIGN KEY ("id_item") REFERENCES "item" ("id");
 
 ALTER TABLE "post_item" ADD FOREIGN KEY ("id_post") REFERENCES "post" ("id");
 
+ALTER TABLE "api_key" ADD FOREIGN KEY ("id_user") REFERENCES "user" ("id");
+
 -- ALTER TABLE "user_roles" ADD FOREIGN KEY ("role_id") REFERENCES "role" ("id");
 
 -- ALTER TABLE "user_roles" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
